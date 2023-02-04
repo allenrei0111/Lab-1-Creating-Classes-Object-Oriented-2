b65130c3fa9d91aa3e56395c31c8aa0c4dbfbba3
allenrei0111 <119450458+allenrei0111@users.noreply.github.com>
2023-02-03 19:36:26 -0700

    Add files via upload

---
+++ Object oriented 2 lab 1/Object oriented 2 lab 1/Person.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Object_oriented_2_lab_1
+{
+    internal class Person
+    {
+        public int personId; 
+        public string firstName;
+        public string lastName; 
+        public string favoriteColour;
+        public int age;
+        public bool isWorking;
+
+        public void DisplayPersonInfo() // persons info//
+        {
+            Console.WriteLine("PersonId: " + personId + " Name: " + firstName + " " + lastName + " Favorite color is " + favoriteColour);
+        }
+
+        public void ChangeFavoriteColour() // change colour//
+        {
+            favoriteColour = "white";
+        }
+
+        public int GetAgeInTenYears() //age after 10 years//
+        {
+            return age + 10;
+        }
+
+        public override string ToString() //displays all Person Object information as a list// 
+        {
+            return "PersonId: " + personId + " \nName: " + firstName + " " + lastName + " \nFavorite color is " + favoriteColour + " \nAge: " + age + " \nisWorking: " + isWorking; //\n displays ouput in different lines//
+        }
+    }
+}
